6fd1425cc6716468580092f6e5ff7ae8e3bda31f
george827 <kinyanjuigeorge827@gmail.com>
2022-11-11 17:55:48 +0300

    add remove and add

--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
-// import _ from 'lodash';
 import './style.css';
 
-// grab items
-const todolist = document.querySelector('.to-do-list');
+import MainFunc from './modules/method.js';
 
-const toDoItems = [
-  {
-    index: 1,
-    description: 'Read the novel',
-    completed: false,
-  },
-  {
-    index: 2,
-    description: 'Play video game with friends',
-    completed: false,
-  },
-  {
-    index: 3,
-    description: 'Go for a movie',
-    completed: false,
-  },
-];
+const userInput = document.querySelector('.add-item');
+const addActivity = document.querySelector('.add-activity');
 
-const dynamic = () => {
-  toDoItems.forEach((list) => {
-    todolist.innerHTML += `
-        <li class="the-item" id="${list.index}">
-            <div class="item-list">
-                <input type="checkbox">
-                <p class="item">${list.description}</p>
-            </div>
-        </li>
-        `;
-  });
-};
+userInput.addEventListener('submit', (e) => {
+  e.preventDefault();
+  MainFunc.addTodo(addActivity.value);
+  addActivity.value = '';
+});
 
-dynamic();
+MainFunc.genList();
